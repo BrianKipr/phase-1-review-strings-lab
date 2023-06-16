ac3db0b6302cab612f2a0f75cd20f8dc99a15fdb
Briankipr <brian.kiprono1@student.moringaschool.com>
2023-06-16 22:07:49 +0300

    phase-1-review-strings-lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Brian';
+const exiteWelcomeMessage = 'WELCOME TO FLATBOOK, '
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const exitedWelcomemessage = welcomeMessage.toUppercase();
+const shortGreeting = `Welcome, ${currentUser.slice(0)}!`;
